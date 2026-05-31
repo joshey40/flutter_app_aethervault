f6b8bda9c8b9e7b61b81bd77a7a4f56ea6d8abfd
Joshua Pieniak <38221036+joshey40@users.noreply.github.com>
2026-05-31 18:53:01 +0200

    Add indexed Scryfall search data source

---
+++ lib/services/scryfall/scryfall_indexed_search_data_source.dart
@@ -0,0 +1,44 @@
+import 'download_service.dart';
+import 'scryfall_card_print.dart';
+import 'scryfall_local_json_search_data_source.dart';
+import 'scryfall_search_repository.dart';
+import 'scryfall_sqlite_search_index.dart';
+import 'bulk_data_type.dart';
+
+class ScryfallIndexedSearchDataSource implements LocalScryfallSearchDataSource {
+  ScryfallIndexedSearchDataSource({
+    DownloadService? downloadService,
+    ScryfallSqliteSearchIndex? searchIndex,
+    ScryfallLocalJsonSearchDataSource? fallbackDataSource,
+    this.maxResults,
+  })  : _downloadService = downloadService ?? DownloadService.instance,
+        _searchIndex = searchIndex ?? ScryfallSqliteSearchIndex.instance,
+        _fallbackDataSource = fallbackDataSource ?? ScryfallLocalJsonSearchDataSource(maxResults: maxResults);
+
+  final DownloadService _downloadService;
+  final ScryfallSqliteSearchIndex _searchIndex;
+  final ScryfallLocalJsonSearchDataSource _fallbackDataSource;
+  final int? maxResults;
+
+  @override
+  Future<List<ScryfallCardPrint>> searchCards({
+    required String rawQuery,
+    required ScryfallBulkDataType type,
+  }) async {
+    final file = await _downloadService.getLocalFile(type: type);
+    if (file == null) {
+      throw StateError('Scryfall ${type.apiType} file is not available.');
+    }
+
+    try {
+      return await _searchIndex.search(
+        type: type,
+        sourceFile: file,
+        rawQuery: rawQuery,
+        maxResults: maxResults,
+      );
+    } on UnsupportedError {
+      return _fallbackDataSource.searchCards(rawQuery: rawQuery, type: type);
+    }
+  }
+}
