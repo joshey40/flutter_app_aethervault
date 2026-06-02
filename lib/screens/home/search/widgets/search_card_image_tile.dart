8ff3ff0bf8f4ed4a2acf17a65ed70d53ccf596c2
Joshua Pieniak <38221036+joshey40@users.noreply.github.com>
2026-06-02 11:26:42 +0200

    Make search cards actionable

--- lib/screens/home/search/widgets/search_card_image_tile.dart
+++ lib/screens/home/search/widgets/search_card_image_tile.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../../services/localization_service.dart';
 import '../../../../services/scryfall/scryfall_card_print.dart';
 import '../../../../theme/app_theme.dart';
 
 class SearchCardImageTile extends StatefulWidget {
-  const SearchCardImageTile({super.key, required this.card});
+  const SearchCardImageTile({
+    super.key,
+    required this.card,
+    this.onTap,
+    this.onAddToDeck,
+  });
 
   final ScryfallCardPrint card;
+  final VoidCallback? onTap;
+  final VoidCallback? onAddToDeck;
 
   @override
   State<SearchCardImageTile> createState() => _SearchCardImageTileState();
@@ -35,43 +43,90 @@ class _SearchCardImageTileState extends State<SearchCardImageTile> {
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(14),
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: placeholderColor,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(isDark ? 0.25 : 0.10),
-                    blurRadius: 12,
-                    offset: const Offset(0, 6),
+      child: Material(
+        color: placeholderColor,
+        child: InkWell(
+          onTap: widget.onTap ?? widget.onAddToDeck,
+          onLongPress: widget.onAddToDeck,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: placeholderColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(isDark ? 0.25 : 0.10),
+                        blurRadius: 12,
+                        offset: const Offset(0, 6),
+                      ),
+                    ],
                   ),
-                ],
+                  child: selectedUrl == null
+                      ? _MissingImageCard(card: card)
+                      : _NetworkCardImage(url: selectedUrl, card: card),
+                ),
               ),
-              child: selectedUrl == null
-                  ? _MissingImageCard(card: card)
-                  : _NetworkCardImage(url: selectedUrl, card: card),
-            ),
-          ),
-          if (imageUrls.length > 1)
-            Positioned(
-              top: 34,
-              right: 7,
-              child: Material(
-                color: Colors.black.withOpacity(0.58),
-                shape: const CircleBorder(),
-                clipBehavior: Clip.antiAlias,
-                child: InkWell(
-                  onTap: () => setState(() => _faceIndex = (_faceIndex + 1) % imageUrls.length),
-                  child: const Padding(
-                    padding: EdgeInsets.all(7),
-                    child: Icon(Icons.flip_rounded, color: Colors.white, size: 18),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.black.withOpacity(0.58),
+                      ],
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 18, 8, 8),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Material(
+                        color: Colors.black.withOpacity(0.62),
+                        shape: const CircleBorder(),
+                        clipBehavior: Clip.antiAlias,
+                        child: InkWell(
+                          onTap: widget.onAddToDeck,
+                          child: Padding(
+                            padding: const EdgeInsets.all(7),
+                            child: Icon(
+                              Icons.playlist_add,
+                              color: Colors.white,
+                              size: 19,
+                              semanticLabel: appLocalizations.translate('decks.addToDeck'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-        ],
+              if (imageUrls.length > 1)
+                Positioned(
+                  top: 34,
+                  right: 7,
+                  child: Material(
+                    color: Colors.black.withOpacity(0.58),
+                    shape: const CircleBorder(),
+                    clipBehavior: Clip.antiAlias,
+                    child: InkWell(
+                      onTap: () => setState(() => _faceIndex = (_faceIndex + 1) % imageUrls.length),
+                      child: const Padding(
+                        padding: EdgeInsets.all(7),
+                        child: Icon(Icons.flip_rounded, color: Colors.white, size: 18),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
